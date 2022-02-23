470ccbffdff68e68d157b555a613d0a46c155c10
msattova <msattova@gmail.com>
2022-02-24 00:39:53 +0900

    コマンドライン引数で入出力ファイルを指定できるように

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 
-
+import modules.arg as arg
 import modules.fileproc as fpc
 
+cmd = arg.CmdArg()
+
 #template = './docx/template.docx'
-template = 'docx/texted_copipe.docx'
-input = './text.txt'
-output = './output.docx'
+template = cmd.args.filename
+input = ''
+output = cmd.args.output
 
 p = fpc.FileProc(template, input, output)
 
